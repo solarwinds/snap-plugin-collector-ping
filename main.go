503437c12cbf3c249cbae656c770222e481abe51
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2020-10-13 18:11:00 +0200

    Configurable plugin

--- main.go
+++ main.go
@@ -18,6 +18,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"sync"
+	"time"
 
 	"github.com/adamiklukasz/go-ping"
 	"github.com/solarwinds/snap-plugin-lib/v2/plugin"
@@ -26,25 +29,80 @@ import (
 
 const (
 	pluginName    = "ping"
-	pluginVersion = "0.0.2"
+	pluginVersion = "0.1.0"
 
-	targetAddr = "www.solarwinds.com"
+	configKey = "config"
 )
+
+type config struct {
+	TargetAddresses []string `json:"target_addresses"`
+	Requests        int      `json:"requests"`
+	IntervalSec     int      `json:"interval_sec"`
+}
 
 type pingCollector struct {
 }
 
-func (c *pingCollector) Collect(ctx plugin.CollectContext) error {
-	dur, err := ping.Ping(targetAddr)
+func (*pingCollector) PluginDefinition(def plugin.CollectorDefinition) error {
+	def.DefineMetric("/solarwinds/ping/avg_time", "ms", true, "Average duration of ping requests")
+	def.DefineMetric("/solarwinds/ping/min_time", "ms", true, "Minimum duration of ping requests")
+	def.DefineMetric("/solarwinds/ping/max_time", "ms", true, "Maximum duration of ping requests")
+	def.DefineMetric("/solarwinds/ping/availability", "", true, "Target address is available via ping [0-no, 1-yes]")
+
+	return nil
+}
+
+func (*pingCollector) Load(ctx plugin.Context) error {
+	var cfg config
+	b := ctx.RawConfig()
+
+	err := json.Unmarshal(b, &cfg)
 	if err != nil {
 		return err
 	}
 
-	durMs := int(float64(dur) / 1000000)
-
-	ctx.AddMetric("/solarwinds/ping/time", durMs, plugin.MetricTag("target", targetAddr), plugin.MetricUnit("ms"))
+	ctx.Store(configKey, cfg)
 
 	return nil
+}
+
+func (*pingCollector) Collect(ctx plugin.CollectContext) error {
+	var cfg config
+	wg := sync.WaitGroup{}
+
+	err := ctx.LoadTo(configKey, &cfg)
+	if err != nil {
+		return err
+	}
+
+	for _, targetAddr := range cfg.TargetAddresses {
+		wg.Add(1)
+
+		go func(targetAddr string) {
+			defer wg.Done()
+
+			pingOk := 1
+			tags := plugin.MetricTag("target", targetAddr)
+
+			stat, err := ping.PingN(targetAddr, cfg.Requests, time.Second*time.Duration(cfg.IntervalSec))
+			if err != nil {
+				pingOk = 0
+			} else {
+				_ = ctx.AddMetric("/solarwinds/ping/avg_time", toMs(stat.Avg), tags)
+				_ = ctx.AddMetric("/solarwinds/ping/min_time", toMs(stat.Min), tags)
+				_ = ctx.AddMetric("/solarwinds/ping/max_time", toMs(stat.Max), tags)
+			}
+
+			_ = ctx.AddMetric("/solarwinds/ping/availability", pingOk, tags)
+		}(targetAddr)
+	}
+
+	wg.Wait()
+	return nil
+}
+
+func toMs(d time.Duration) int {
+	return int(float64(d) / 1000000)
 }
 
 func main() {
